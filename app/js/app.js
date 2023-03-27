2dc1da26e90edc404cbab096c8d4867df33e8326
evgenij <mwbox89@gmail.com>
2023-03-27 21:06:13 +0300

    some fix

--- app/js/app.js
+++ app/js/app.js
@@ -1,32 +1,35 @@
-
 document.addEventListener('DOMContentLoaded', () => {
 
 	// Custom JS
-	stickyHeader ()
-	mobMenuToggle ()
-	accordion ('.accordion-item-js', {closeItem: false})
+	stickyHeader()
+	mobMenuToggle()
+	accordion('.accordion-item-js', {
+		closeItem: false
+	})
 	videoPopup()
-	smoothScroll ()
-	switchPrice ()
-          
-})
+	switchPrice()
 
+})
+window.addEventListener('load', function () {
+	smoothScroll()
+})
 // мобильное меню
-function mobMenuToggle () {
+function mobMenuToggle() {
 	let btn = document.querySelector('.header__navigation-btn-menu')
 	let menu = document.querySelector(btn.dataset.toggle)
 	let header = document.querySelector('.header')
-		btn.addEventListener('click', function (e) {
-			btn.classList.toggle('active')
-			menu.classList.toggle('active')
-			header.classList.toggle('active')
-		})
+	btn.addEventListener('click', function (e) {
+		btn.classList.toggle('active')
+		menu.classList.toggle('active')
+		header.classList.toggle('active')
+	})
 }
-function accordion (accordionItemSelector, mode) {
+
+function accordion(accordionItemSelector, mode) {
 	let accordionElements = document.querySelectorAll(accordionItemSelector)
 	let openedElement
-	for(let i = 0; i < accordionElements.length; i++) {
-		
+	for (let i = 0; i < accordionElements.length; i++) {
+
 		if (accordionElements[i].classList.contains('open')) {
 			openedElement = accordionElements[i]
 		}
@@ -53,8 +56,9 @@ function accordion (accordionItemSelector, mode) {
 
 		})
 	}
-}	
-function stickyHeader () {
+}
+
+function stickyHeader() {
 	let header = document.querySelector('.header')
 
 	if (document.body.getBoundingClientRect().top < 0) {
@@ -62,19 +66,19 @@ function stickyHeader () {
 	} else {
 		header.classList.remove('sticky')
 	}
-	
+
 	document.addEventListener('scroll', function () {
 		if (document.body.getBoundingClientRect().top < 0) {
 			header.classList.add('sticky')
 		} else {
 			header.classList.remove('sticky')
 		}
-		
+
 	})
 }
 
 
-function videoPopup () {
+function videoPopup() {
 	let btn = document.querySelector('a[href*="www.youtube.com"]')
 
 	if (!btn) {
@@ -90,9 +94,9 @@ function videoPopup () {
 		let popupInner = popup.querySelector('.popup__inner')
 		let frame = document.createElement('iframe')
 		frame.classList.add('popup__frame')
-        frame.src = "https://www.youtube.com/embed/"+videoId
-        frame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
-        frame.setAttribute('allowfullscreen', true)
+		frame.src = "https://www.youtube.com/embed/" + videoId
+		frame.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
+		frame.setAttribute('allowfullscreen', true)
 		popupInner.appendChild(frame)
 	})
 
@@ -104,42 +108,42 @@ function videoPopup () {
 	})
 }
 
-function smoothScroll () {
+function smoothScroll() {
 	let linkNav = document.querySelectorAll('[href^="#"]')
 	let headerHeight = document.querySelector('.header').getBoundingClientRect().height
 	let V = 0.2;
 	for (let i = 0; i < linkNav.length; i++) {
-		linkNav[i].addEventListener('click', function(e) { //по клику на ссылку
+		linkNav[i].addEventListener('click', function (e) { //по клику на ссылку
 			e.preventDefault(); //отменяем стандартное поведение
-			let w = window.pageYOffset  // производим прокрутка прокрутка
+			let w = window.pageYOffset // производим прокрутка прокрутка
 			let hash = this.href.replace(/[^#]*(.*)/, '$1');
 			let tar = document.querySelector(hash) // к id элемента, к которому нужно перейти
-			let t = tar.getBoundingClientRect().top - headerHeight  // отступ от окна браузера до id
+			let t = tar.getBoundingClientRect().top - headerHeight // отступ от окна браузера до id
 			let start = null;
-			requestAnimationFrame(step);  // подробнее про функцию анимации [developer.mozilla.org]
+			requestAnimationFrame(step); // подробнее про функцию анимации [developer.mozilla.org]
 			function step(time) {
 				if (start === null) {
 					start = time;
 				}
 				var progress = time - start,
-				r = (t < 0 ? Math.max(w - progress/V, w + t) : Math.min(w + progress/V, w + t));
-				window.scrollTo(0,r);
+					r = (t < 0 ? Math.max(w - progress / V, w + t) : Math.min(w + progress / V, w + t));
+				window.scrollTo(0, r);
 				if (r != w + t) {
 					requestAnimationFrame(step)
 				} else {
-					location.hash = hash  // URL с хэшем
+					location.hash = hash // URL с хэшем
 				}
 			}
 		}, false);
-}
+	}
 }
 
-function switchPrice () {
+function switchPrice() {
 	let form = document.querySelector('.switch')
 	if (!form) return
 	let prices = document.querySelectorAll('.js-change-price')
 	form.addEventListener('change', function (e) {
-		for(let i = 0; i < prices.length; i++) {
+		for (let i = 0; i < prices.length; i++) {
 			prices[i].innerHTML = prices[i].dataset[e.target.value] + '$'
 		}
 	})
